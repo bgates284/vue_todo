04e85be6a433bf45085a6a8b10b153d8274762ff
Bill Gates <William.Gates@promachbuilt.com>
2024-02-08 14:33:49 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return a.createdAt - b.createdAt
+}))
+
+watch(name, (newVal => {
+  localStorage.setItem('name', newVal)
+}))
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
+</script>
+
+<template>
+  <main class = "app">
+    <section class = "greeting">
+      <h2 class = "title">
+        What's up, <input type="text" placeholder="Name here "
+        v-model = "name"/>
+      </h2>
+    </section>
+  </main>
+</template>
+
+
